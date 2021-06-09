37e9da6e4dbc3e4c80be2e1ab0d7dc54ac93a410
mrm <matt.rmoore@gmail.com>
2021-06-08 23:47:02 -0500

    Initial commit for starter code for class on 9 June 2021.

---
+++ Classes/2021-06-09/05_pagila_orderby_starter.sql
@@ -0,0 +1,20 @@
+/*
+## Movies Ordered By
+In this activity, you will use `ORDER BY` in combination with other SQL methods to query and order the tables.
+
+### Instructions
+* Determine the count of actor first names ordered in descending order.
+* Determine the average rental duration for each rating rounded to two decimals. Order these in ascending order.
+* Determine the top 10 average replace costs for length of the movie. 
+
+### Bonus
+* Using the city and country tables, determine the number of cities in each country from the database in descending order.
+*/
+
+-- Select count of actors first names in descending order
+
+-- Select the average duration of movies by rating
+
+-- Select top ten replace costs for the length of the movie
+
+-- Select the count of countries
