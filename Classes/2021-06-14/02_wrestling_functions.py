6284ccf6b74daa22f809a70a860b9dcdaa4f3159
mrm <matt.rmoore@gmail.com>
2021-06-15 20:04:26 -0500

    Completed work from class on 14 June.

--- Classes/2021-06-14/02_wrestling_functions.py
+++ Classes/2021-06-14/02_wrestling_functions.py
@@ -1,19 +1,39 @@
+"""
+# Wrestling with Functions
+
+## Instructions
+
+* Analyze the code and CSV provided, looking specifically for what needs to still be added to the application.
+
+* Using the starter code provided, create a function called `print_percentages` which takes in a parameter called `wrestler_data` and does the following:
+
+  * Uses the data stored within `wrestler_data` to calculate the percentage of matches the wrestler won, lost, and drew over the course of a year.
+
+  * Prints out the stats for the wrestler to the terminal.
+
+## Bonus
+
+* Still within the `print_percentages()` function, create a conditional that checks a wrestler's loss percentage and prints either "Jobber" to the screen if the number was greater than fifty or "Superstar" if the number was less than 50.
+"""
+
 import os
 import csv
 
 # Path to collect data from the Resources folder
+wrestling_csv = os.path.join("Resources", "WWE-Data-2016.csv")
 
 # Define the function and have it accept the 'wrestlerData' as its sole parameter
-
-# Find the total number of matches wrestled
-
-# Find the percentage of matches won
-
-# Find the percentage of matches lost
-
-# Find the percentage of matches drawn
-
-# Print out the wrestler's name and their percentage stats
+def print_percentages(wrestlerData):
+    # Find the total number of matches wrestled
+    total_matches = int(wrestlerData[1]) + int(wrestlerData[2]) + int(wrestlerData[3])
+    # Find the percentage of matches won
+    percent_won = (int(wrestlerData[1]) / total_matches) * 100
+    # Find the percentage of matches lost
+    percent_lost = (int(wrestlerData[2]) / total_matches) * 100
+    # Find the percentage of matches drawn
+    percent_draw = (int(wrestlerData[3]) / total_matches) * 100
+    # Print out the wrestler's name and their percentage stats
+    print(f'{wrestlerData[0]} has won {percent_won:.2f} percent of matches, lost {percent_lost:.2f} percent of matches and drawn {percent_draw:.2f} percent of matches.')
 
 # Read in the CSV file
 with open(wrestling_csv, 'r') as csvfile:
