422d512ee428c2e27cb35b5301aa6693f040d6cb
mrm <matt.rmoore@gmail.com>
2021-06-18 23:03:50 -0500

    Challenge 7, Deliverable 2 complete.

--- Challenges/Challenge07/Employee_Database_challenge.sql
+++ Challenges/Challenge07/Employee_Database_challenge.sql
@@ -27,3 +27,13 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 -- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, d.from_date, d.to_date, t.title
+INTO challenge_mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_no AS d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
