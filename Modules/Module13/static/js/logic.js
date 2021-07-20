e7136af6038437374e6a3d07cea13993fd8fb6fd
mrm <matt.rmoore@gmail.com>
2021-07-19 22:55:56 -0500

    Polyline complete.

--- Modules/Module13/static/js/logic.js
+++ Modules/Module13/static/js/logic.js
@@ -2,10 +2,10 @@
 console.log("working");
 
 // Create a map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Set tile layer using Leaflet intro code
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,10 +20,32 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
 //  }).addTo(map);
 
 // Use CircleMarker function
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 150,
-    color: "black",
-    fillColor: "#ffffa1"
+// let marker = L.circleMarker([34.0522, -118.2437], {
+//     radius: 150,
+//     color: "black",
+//     fillColor: "#ffffa1"
+// }).addTo(map);
+
+// let line = [
+//     [33.9416, -118.4085],  // 1st point
+//     [37.6213, -122.3790],  // 2nd point
+//     [40.7899, -111.9791],  // 3rd point
+//     [47.4502, -122.3088]   // 4th point
+// ];
+
+// skill drill
+let line = [
+    [39.3036, -94.7093],  // 1st point - MCI
+    [40.6413, -73.7781],  // 2nd point - JKF
+    [36.0840, -115.1537],  // 3rd point - McCarran
+    [39.3036, -94.7093]   // 4th point - 
+];
+
+
+L.polyline(line, {
+    color: "blue",
+    dashArray: "20",
+    dashOffset: "20"
 }).addTo(map);
 
 // Add 'graymap' tile layer to the map.
