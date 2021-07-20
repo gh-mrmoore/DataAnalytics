545dd043a7f1ad3234662b7b99e9ddb171a42665
mrm <matt.rmoore@gmail.com>
2021-07-19 23:55:11 -0500

    Module 13 complete up to 13.5.3

--- Modules/Module13/static/js/logic.js
+++ Modules/Module13/static/js/logic.js
@@ -2,28 +2,50 @@
 console.log("working");
 
 // Create a map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
 
 // Set tile layer using Leaflet intro code
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Add a marker to the map for Los Angeles
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Add GeoJSON data. Copied from Module.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Add circle to the map around downtown LA
-// let marker = L.circle([34.0522, -118.2437], {
-//     radius: 10000 // re-sized to be more visible
-//  }).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h3>" + feature.properties.name + "</h3><hr /><h4>" + feature.properties.city + "</h4>");
+//     }
+// }).addTo(map);
 
-// Use CircleMarker function
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 150,
-    color: "black",
-    fillColor: "#ffffa1"
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h3>" + feature.properties.name + "</h3><hr /><h4>" + feature.properties.city + "</h4>");
+    }
 }).addTo(map);
 
 // Add 'graymap' tile layer to the map.
