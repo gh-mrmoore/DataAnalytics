1e0fb504d622b1e77075fb378f80e345f1ebac55
mrm <matt.rmoore@gmail.com>
2021-07-20 21:44:13 -0500

    Commit to get URL for data file.

--- Modules/Module13/static/js/logic.js
+++ Modules/Module13/static/js/logic.js
@@ -1,8 +1,5 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create a map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Set tile layer using Leaflet intro code
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,20 +8,32 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
-// Add a marker to the map for Los Angeles
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Light mode
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Add circle to the map around downtown LA
-// let marker = L.circle([34.0522, -118.2437], {
-//     radius: 10000 // re-sized to be more visible
-//  }).addTo(map);
+// Dark Mode
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Use CircleMarker function
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 150,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+let map = L.map("mapid", {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [light]
+});
 
-// Add 'graymap' tile layer to the map.
-streets.addTo(map);
+let baseMaps = {
+    Light: light,
+    Dark: dark
+};
+
+// Get route data
+let torontoRoutes = "";
+
+// Add route data to map
