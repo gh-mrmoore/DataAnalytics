fc0e4be01c3da00fbe9bc7a377b299fc1d938208
mrm <matt.rmoore@gmail.com>
2021-07-20 22:42:51 -0500

    Module 13 complete thru 13.5.6

--- Modules/Module13/static/js/logic.js
+++ Modules/Module13/static/js/logic.js
@@ -1,9 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-// Create a map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
 // Set tile layer using Leaflet intro code
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,20 +5,43 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
-// Add a marker to the map for Los Angeles
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Add circle to the map around downtown LA
-// let marker = L.circle([34.0522, -118.2437], {
-//     radius: 10000 // re-sized to be more visible
-//  }).addTo(map);
+let map = L.map("mapid", {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
+});
 
-// Use CircleMarker function
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 150,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+let baseMaps = {
+    Street: streets,
+    Satellite: satelliteStreets
+};
 
-// Add 'graymap' tile layer to the map.
-streets.addTo(map);
+// set data URL
+let torontoNh = "https://raw.githubusercontent.com/gh-mrmoore/DataAnalytics/PolygonGeoJSON/Modules/Module13/static/js/torontoNeighborhoods.json";
+
+// Simplify styling with an object
+let mapStyle = {
+    color: "yellow",
+    weight: 1
+};
+
+// Get data and map it
+d3.json(torontoNh).then(function(data) {
+    console.log(data);
+    L.geoJSON(data, {
+        style: mapStyle,
+        onEachFeature: function(feature, layer) {
+            console.log(layer);
+            layer.bindPopup("Welcome to " + feature.properties.AREA_NAME);
+        }
+    }).addTo(map);
+});
+
+// Pass and add
+L.control.layers(baseMaps).addTo(map);
