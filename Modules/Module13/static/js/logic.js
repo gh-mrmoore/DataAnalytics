c2001e5a6f17e3b0d2e0139b1c446875c8aa48b2
mrm <matt.rmoore@gmail.com>
2021-07-20 21:35:10 -0500

    Module 13 complete up to 13.5.5

--- Modules/Module13/static/js/logic.js
+++ Modules/Module13/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create a map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Set tile layer using Leaflet intro code
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,20 +11,49 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
-// Add a marker to the map for Los Angeles
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Create map object with each attribute
+let map = L.map("mapid", {
+    center: [40.7, -94.5],
+    zoom: 2,
+    layers: [streets]
+});
 
-// Add circle to the map around downtown LA
-// let marker = L.circle([34.0522, -118.2437], {
-//     radius: 10000 // re-sized to be more visible
-//  }).addTo(map);
+// Add a dark mode to the map
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Use CircleMarker function
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 150,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+// Base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// Get airport GeoJSON from URL
+let airportData = "https://raw.githubusercontent.com/gh-mrmoore/DataAnalytics/MultipleGeoJSON/Modules/Module13/static/js/majorAirports.json";
+
+// Get data using d3
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//     // create GeoJSON layer
+//     L.geoJSON(data).addTo(map);
+// });
+
+// Re-factor to include popup
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    L.geoJSON(data, {
+        onEachFeature: function(feature, layer) {
+            console.log(layer);
+            layer.bindPopup("City: " + feature.properties.city)
+        }
+    }).addTo(map);
+})
 
 // Add 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
+
+// Pass map layer to layer control and add layer control
+L.control.layers(baseMaps).addTo(map);
