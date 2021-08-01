45de31080d9a59afec41dde7f1d724c0a9ce2f81
mrm <matt.rmoore@gmail.com>
2021-08-01 15:03:02 -0500

    Deliverable 1 thru 3 done except for formatting.

--- Challenges/Challenge13/static/js/logic.js
+++ Challenges/Challenge13/static/js/logic.js
@@ -26,10 +26,12 @@ let baseMaps = {
 // Set links to get the geojson data.
 var tectonicData = "static/data/PB2002_boundaries.geojson";
 var earthQuakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var majorQuakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 // Create layers
 let quakeLayer = new L.layerGroup();
 let plateLayer = new L.layerGroup();
+let majorLayer = new L.layerGroup();
 
 let map = L.map("map", {
     center: [42.36, -71.06],
@@ -39,18 +41,26 @@ let map = L.map("map", {
 
 let overlays = {
     "Earthquakes": quakeLayer,
+    "Major Earthquakes": majorLayer,
     "Tectonic Plates": plateLayer
 };
 
 // Get our GeoJSON data using d3.json
 // add the data to the appropriate layer, not the map itself
 d3.json(tectonicData, function(data) {
-    L.geoJSON(data).addTo(plateLayer)
+    L.geoJSON(data, {
+        style: {color: "red", fillColor: "red", fillOpactiy: 1.0, weight: 1.5},
+    }).addTo(plateLayer)
 });
 
 // layers will be added to the map via the control.
 d3.json(earthQuakeData, function(data) {
     L.geoJSON(data).addTo(quakeLayer)
+});
+
+// Add layer for M4.5+ Earthquakes for last 7 days
+d3.json(majorQuakeData, function(data) {
+    L.geoJSON(data).addTo(majorLayer)
 });
 
 L.control.layers(baseMaps, overlays).addTo(map);
