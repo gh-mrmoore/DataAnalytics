d5afd5abc4b6b66e044166e64a996257e1f2be5c
mrm <matt.rmoore@gmail.com>
2021-08-01 14:33:12 -0500

    Deliverable 1 and 3 mostly done except formatting.

---
+++ Challenges/Challenge13/static/js/logic.js
@@ -0,0 +1,56 @@
+// Set tile layer using Leaflet intro code
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let nightStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets,
+    "Nights": nightStreets
+};
+
+// Set links to get the geojson data.
+var tectonicData = "static/data/PB2002_boundaries.geojson";
+var earthQuakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Create layers
+let quakeLayer = new L.layerGroup();
+let plateLayer = new L.layerGroup();
+
+let map = L.map("map", {
+    center: [42.36, -71.06],
+    zoom: 11,
+    layers: [streets]
+});
+
+let overlays = {
+    "Earthquakes": quakeLayer,
+    "Tectonic Plates": plateLayer
+};
+
+// Get our GeoJSON data using d3.json
+// add the data to the appropriate layer, not the map itself
+d3.json(tectonicData, function(data) {
+    L.geoJSON(data).addTo(plateLayer)
+});
+
+// layers will be added to the map via the control.
+d3.json(earthQuakeData, function(data) {
+    L.geoJSON(data).addTo(quakeLayer)
+});
+
+L.control.layers(baseMaps, overlays).addTo(map);
